4affad5fd4257810933d9b5bebd762dae0bd4732
neiro <neiro.mail@gmail.com>
2016-03-15 15:11:22 +0300

    Fix code style

--- lib/argo/guardian_serializer.ex
+++ lib/argo/guardian_serializer.ex
@@ -4,9 +4,9 @@ defmodule Argo.GuardianSerializer do
   alias Argo.Repo
   alias Argo.User
 
-  def for_token(user = %User{}), do: { :ok, "User:#{user.id}" }
-  def for_token(_), do: { :error, "Unknown resource type" }
+  def for_token(user = %User{}), do: {:ok, "User:#{user.id}"}
+  def for_token(_), do: {:error, "Unknown resource type"}
 
-  def from_token("User:" <> id), do: { :ok, Repo.get(User, id) }
-  def from_token(_), do: { :error, "Unknown resource type" }
+  def from_token("User:" <> id), do: {:ok, Repo.get(User, id)}
+  def from_token(_), do: {:error, "Unknown resource type"}
 end
