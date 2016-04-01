538a4894d892e3a755c72cf42d3c055d69d1e321
neiro <neiro.mail@gmail.com>
2016-04-01 22:42:22 +0300

    Add sign in form

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -5,9 +5,24 @@ defmodule Argo.SessionController do
 
   use Argo.Web, :controller
 
-  alias MyApp.User
+  alias Argo.User
+
+  plug :scrub_params, "user" when action in [:create]
 
   def new(conn, _params) do
     render conn, "new.html"
+  end
+
+  def create(conn, _params = %{}) do
+    conn
+      |> put_flash(:info, "Logged in.")
+      # |> Guardian.Plug.sign_in(verified_user) # verify your logged in resource
+      |> redirect(to: "/")
+  end
+
+  def destroy(conn, _params) do
+    Guardian.Plug.sign_out(conn)
+      |> put_flash(:info, "Logged out successfully.")
+      |> redirect(to: "/")
   end
 end
