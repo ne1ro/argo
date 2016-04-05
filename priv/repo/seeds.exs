9dbe86d9191a7b327ca85a5c36708f6d33311576
neiro <neiro.mail@gmail.com>
2016-04-05 22:17:52 +0300

    Create seed for admin user

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -5,7 +5,16 @@
 # Inside the script, you can read and write to any of your
 # repositories directly:
 #
-#     Argo.Repo.insert!(%Argo.SomeModel{})
+pass = System.get_env "ADMIN_PASSWORD"
+
+Argo.Repo.insert! %Argo.User{
+  name: "admin",
+  email: System.get_env("ADMIN_EMAIL"),
+  admin: true,
+  password: pass,
+  password_confirmation: pass
+}
+
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
