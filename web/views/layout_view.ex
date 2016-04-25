01bd82e0b1c48c30a2ac68cfe475dd8dd7d1c5c6
neiro <neiro.mail@gmail.com>
2016-04-25 22:18:25 +0300

    Change logged helpers

--- web/views/layout_view.ex
+++ web/views/layout_view.ex
@@ -5,5 +5,9 @@ defmodule Argo.LayoutView do
 
   use Argo.Web, :view
 
+  def logged_as_admin?(conn), do: Guardian.Plug.authenticated?(conn, :admin)
+  def admin_user(conn), do: Guardian.Plug.current_resource(conn, :admin)
+
+  def logged_in?(conn), do: Guardian.Plug.authenticated?(conn)
   def current_user(conn), do: Guardian.Plug.current_resource(conn)
 end
